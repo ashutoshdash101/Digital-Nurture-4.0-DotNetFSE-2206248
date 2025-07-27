fe879e775281c2300dd4099b312e114accc3f11a
ashutoshdash101 <ashutoshpiku1234@gmail.com>
2025-07-27 22:20:45 +0530

    Add Files in week_6

---
+++ week_6/React 3/src/App.js
@@ -0,0 +1,15 @@
+import { CalculateScore } from '../src/Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={" Ashutosh Dash"}
+        School={" Jawahar Navodaya Vidyalaya"}
+        total={ 289}
+        goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
