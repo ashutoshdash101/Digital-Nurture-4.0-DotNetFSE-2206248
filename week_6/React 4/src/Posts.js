24658c40096399748c9d4b4848d59d31707aeb44
ashutoshdash101 <ashutoshpiku1234@gmail.com>
2025-07-27 22:21:09 +0530

    Add Files in week_6

---
+++ week_6/React 4/src/Posts.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data.slice(0, 5) })) // load only first 5 posts
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred while loading posts.");
+    console.error("Error caught in componentDidCatch:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2 style={{ color: 'red' }}>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
